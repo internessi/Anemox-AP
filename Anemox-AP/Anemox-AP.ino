37bbb8e1d7b1a8dea0fb56adf662c9e4be544f79
internessi <internessi@users.noreply.github.com>
2019-09-04 11:27:40 +0200

    Update Anemox-AP.ino

--- Anemox-AP/Anemox-AP.ino
+++ Anemox-AP/Anemox-AP.ino
@@ -6,6 +6,8 @@
 #include <SPIFFS.h>
 #include <Wire.h> 
 #include <RtcDS1307.h>
+#include <subs.h>
+
 
 
 #define dataPin  27
@@ -157,20 +159,4 @@ void loop() {
 
     delay(15000);
     
-}
-
-void printDateTime(const RtcDateTime& dt)
-{
-    char datestring[20];
-
-    snprintf_P(datestring, 
-            countof(datestring),
-            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
-            dt.Month(),
-            dt.Day(),
-            dt.Year(),
-            dt.Hour(),
-            dt.Minute(),
-            dt.Second() );
-    Serial.print(datestring);
 }
